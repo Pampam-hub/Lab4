0ba0ac99c20ec2bf0c5572b893677e227c907f55
Pampam-hub <80539642+Pampam-hub@users.noreply.github.com>
2022-01-21 13:59:29 +0300

    Update AreaType.java

--- src/enums/AreaType.java
+++ src/enums/AreaType.java
@@ -1,5 +1,20 @@
 package enums;
 
 public enum AreaType {
-    TONAL, CHASM, SKY, TOWN, STREET, VILLAGE, EARTH
+    TONAL("Тоннель"),
+    CHASM("Пропасть"),
+    SKY("Небо"),
+    TOWN("Город"),
+    STREET("Улица"),
+    VILLAGE("Окраина");
+
+    private final String name;
+
+    AreaType(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
