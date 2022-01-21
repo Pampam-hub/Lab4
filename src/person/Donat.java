6a5977d01065c0a3841ca7843a982960e6b8175d
Pampam-hub <80539642+Pampam-hub@users.noreply.github.com>
2022-01-21 11:06:16 +0300

    Update Donat.java

--- src/person/Donat.java
+++ src/person/Donat.java
@@ -1,11 +1,1 @@
-package person;
 
-public class Donat extends Person {
-    public Donat(String name) {
-        super(name);
-    }
-
-    public void getLost() {
-        System.out.printf("%s заблудился между ледяными колоннами.\n", getName());
-    }
-}
